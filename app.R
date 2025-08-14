8bb1db0cb6309ad0fd27672d4754d801434d820a
iapich <pichler.ian@virology.uzh.ch>
2025-08-14 12:16:17 +0200

    adjustments to the plot

--- app.R
+++ app.R
@@ -28,16 +28,17 @@ server <- function(input, output) {
     
     title_text <- sprintf("%s (%d nt)", data()[1, 1], total_rows)
     
-    plot <- ggplot(dd, aes(x = X2, y = X3)) +
+    ggplot(data(), aes(x = V2, y = V3)) +
       geom_line() +
       scale_y_log10(
-        limits = c(1e-1, 1e4),  # ≈ 0.1 to 10,000
-        breaks = c(0, 1, 10, 100, 1000, 10000),  # include 0 as a fake label
-        labels = function(x) ifelse(x == 0, "0", x),
+        limits = c(1, 1e4),  # fixed lower & upper limits
+        breaks = c(1, 10, 100, 1000, 10000),
+        labels = function(x) ifelse(x == 1, "0", x),  # fake label at the bottom
         minor_breaks = rep(1:9, 21) * (10^rep(-10:10, each = 9))
       ) +
       theme_minimal() +
       labs(
+        title = title_text,
         subtitle = paste(
           "Horizontal coverage:", paste0(horizontal, "%"),
           "| Mean vertical coverage:", paste0(vertical, "X")
@@ -46,8 +47,6 @@ server <- function(input, output) {
         y = "Coverage (reads)"
       ) +
       theme(plot.title = element_text(face = "bold"))
-    
-    print(plot)
   })
   
   output$downloadPlot <- downloadHandler(
@@ -55,10 +54,10 @@ server <- function(input, output) {
       paste("plot_", Sys.Date(), ".pdf", sep = "")
     },
     content = function(file) {
-      ggsave(file, plot = last_plot(), device = "pdf", width = 10, height = 6, units = "in")
+      ggsave(file, plot = last_plot(), device = "pdf",
+             width = 10, height = 6, units = "in")
     }
   )
 }
 
 shinyApp(ui, server)
-
